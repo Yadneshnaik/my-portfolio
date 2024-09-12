47a180fa0f134ca6a2614d9992508946e012f2ba
Yadneshnaik <naikyadnesh9@gmail.com>
2024-09-12 15:13:52 +0530

    init

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,14 +5,15 @@ const Projects = () => {
   return (
     <section className="projects-section">
       <h2>Projects</h2>
-      <div className="projects-list">
-        <div className="project-item">
-          <a href="https://infrenox-pvt-ltd.vercel.app/" target="_blank" rel="noopener noreferrer">
-            <h3>Infrenox Pvt Ltd.</h3>
-            <p>At Infrenox Pvt Ltd, we specialize in delivering innovative IT solutions tailored to meet the unique needs of businesses across industries.</p>
-          </a>
+      <div className="projects-container">
+        <div className="projects-list">
+          <div className="project-item">
+            <a href="https://infrenox-pvt-ltd.vercel.app/" target="_blank" rel="noopener noreferrer">
+              <h3>Infrenox Pvt Ltd.</h3>
+              <p>At Infrenox Pvt Ltd, we specialize in delivering innovative IT solutions tailored to meet the unique needs of businesses across industries.</p>
+            </a>
+          </div>
         </div>
-        {/* Add more projects */}
       </div>
     </section>
   );
